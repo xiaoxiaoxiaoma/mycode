2afdd5d76c919d5f121bc35699f22cd139ac7f4d
xiaoma <mayangrong@tungee.com>
2018-08-13 12:34:42 +0800

    Transform.py from nowcoder

--- nowcoder/Transform.py
+++ nowcoder/Transform.py
@@ -2,12 +2,18 @@
 class Transform:
     def transformImage(self, mat, n):
         # write code here
-        m=n
-        for i in range(n-1,-1,-1):
-            for j in range(n):
-                mat[m].append(mat[i][j])
-            m+=1
+
+        #把列表倒排一遍
+        #b=['1','2','3','4','5','6']
+        #for i in range(len(b)-1,-1,-1):
+        #b.append(b[i])
+        mat.reverse()
+
         for i in range(n):
-            mat.pop(0)
+            for j in range(i, n):
+                if i == j:
+                    pass
+                else:
+                    mat[i][j], mat[j][i] = mat[j][i], mat[i][j]
+
         return mat
-# I need more time to solve it
